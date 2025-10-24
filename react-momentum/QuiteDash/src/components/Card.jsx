955d04fe44da19c114c01ce9c0c6fcbfb31d26af
USER <aishamuarin@gmail.com>
2025-10-24 22:47:43 +0100

    test image profile test on card

--- react-momentum/QuiteDash/src/components/Card.jsx
+++ react-momentum/QuiteDash/src/components/Card.jsx
@@ -1,5 +1,6 @@
 import ProfileCard from "./ProfileCard"
 import { FaUser } from "react-icons/fa"
+//import profile from "../assets/Copy of IMG_2303.jpg"
 
 
 function Card() {
@@ -8,11 +9,12 @@ function Card() {
       <div className="max-w-9/12 h-32 shadow rounded flex p-3 items-center bg-gray-100 gap-3">
       <div>
         <FaUser className="text-white text-7xl bg-[#1F2937] rounded-full p-3" />
+       {/*  <img src={profile} alt="" className="w-30 h-30  rounded-full p-3" /> */}
       </div>
       <ProfileCard 
       name="Humiara Muhyiddeen"
       role="Junior Developer"
-      status="Frontend Development"
+      track="Frontend Development"
       skill={"Html, JavaScript, TailwindCss, React"}
       joined={"October 2024"} 
       FocusArea={"Learning React"}/>
