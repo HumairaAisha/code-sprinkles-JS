43601f244ee1a11db64f522bdb7d062b11436e0f
USER <aishamuarin@gmail.com>
2025-10-25 15:43:54 +0100

    build layout to correspond to sidebar behavior on pages

--- react-momentum/QuiteDash/src/components/layout/Layout.jsx
+++ react-momentum/QuiteDash/src/components/layout/Layout.jsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom"
 import SideBar from "./SideBar"
 import Header from './Header'
 
+import { useState } from "react"
 function Layout() {
+  const [isOpen, setIsOpen] = useState(true)
   return (
     <div className="flex bg-[#F3F4F6]">
-      <SideBar className="w-48" />
+      <SideBar isOpen={isOpen} setIsOpen={setIsOpen}
+       className="w-48" />
 
-      <div className="flex-1 ml-48">
-        <Header className=""/>
+      <div className={`${isOpen ? 'ml-48' : 'ml-16'} flex-1`}>
+        <Header isOpen={isOpen}/>
       <div className="flex-1 mt-12 bg-[#F3F4F6]">
         <Outlet />
       </div>
