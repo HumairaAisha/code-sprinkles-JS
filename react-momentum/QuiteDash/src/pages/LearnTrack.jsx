c4ebfa5bf02abfc82dcfa82142375d4130a9a862
USER <aishamuarin@gmail.com>
2025-10-25 18:12:58 +0100

    add the learning page layout

--- react-momentum/QuiteDash/src/pages/LearnTrack.jsx
+++ react-momentum/QuiteDash/src/pages/LearnTrack.jsx
@@ -1,9 +1,25 @@
-
+//import { useState } from "react"
 
 function LearnTrack() {
+  
   return (
     <div className="h-screen bg-[#F3F4F6] p-4">
-      LearnTrack</div>
+      <div>
+        <div className="bg-[#0f1a32] rounded-lg text-white p-2 m-2">
+          <h3 className="font-bold text-lg">Your Learning Footprints</h3>
+          <p className="text-sm my-1">Log learnning progress, track hours spent, and see your growth over time, with ideas that shape your journey. <br />
+            Every new entry builds a record of your learning story.</p>
+          <div className="flex justify-end px-4 items-center -my-2 mb-1">
+             <button className="bg-white text-[#0F172A] px-1.5 py-1 rounded hover:cursor-pointer">Note It</button>
+          </div>
+       
+        </div>
+        <div>
+         
+         
+        </div>
+      </div>
+     </div>
   )
 }
 
