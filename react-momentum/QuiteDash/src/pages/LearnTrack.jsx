a80a41b7be9dafd27ba933c99c3386a59926b12e
USER <aishamuarin@gmail.com>
2025-10-26 10:21:10 +0100

    add pop modal logic to button

--- react-momentum/QuiteDash/src/pages/LearnTrack.jsx
+++ react-momentum/QuiteDash/src/pages/LearnTrack.jsx
@@ -1,7 +1,14 @@
-//import { useState } from "react"
+import { useState } from "react"
+import MyForm from "../components/Form/MyForm"
+import Modal from "../components/Form/Modal"
+
 
 function LearnTrack() {
-  
+  const [openModal, setOpenModal] = useState(false)
+
+  const openForm = () => setOpenModal(true)
+  const closeForm = () => setOpenModal(false)
+
   return (
     <div className="h-screen bg-[#F3F4F6] p-4">
       <div>
@@ -10,7 +17,13 @@ function LearnTrack() {
           <p className="text-sm my-1">Log learnning progress, track hours spent, and see your growth over time, with ideas that shape your journey. <br />
             Every new entry builds a record of your learning story.</p>
           <div className="flex justify-end px-4 items-center -my-2 mb-1">
-             <button className="bg-white text-[#0F172A] px-1.5 py-1 rounded hover:cursor-pointer">Note It</button>
+             <button onClick={openForm}
+             className="bg-white text-[#0F172A] px-1.5 py-1 rounded font-semibold hover:cursor-pointer">Note It</button>
+             {openModal && (
+              <Modal onClose={closeForm}>
+                <MyForm /> 
+              </Modal>
+             )}
           </div>
        
         </div>
