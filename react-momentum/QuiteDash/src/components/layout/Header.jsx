0d85d0feac7a21389bbb4fee7c0f849a2dd30d6a
USER <aishamuarin@gmail.com>
2025-11-01 14:41:01 +0100

    feat:update and change challenge page name

--- react-momentum/QuiteDash/src/components/layout/Header.jsx
+++ react-momentum/QuiteDash/src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ import { useLocation } from 'react-router-dom';
 function Header({isOpen}) {
   const location = useLocation()
 
-  const pageTitle = {"/dashboard": "Dashboard", "/learn": "Learning Tracker", "/milestone": "Milestone", "/resources": "Resources", "/settings": "Settings", "/profile": "Profile"}
+  const pageTitle = {"/dashboard": "Dashboard", "/learn": "Learning Tracker", "/challenge" : "Challenge Journal", "/milestone": "Milestone", "/resources": "Resources", "/settings": "Settings", "/profile": "Profile"}
   const {pathname} = location
   const title = pageTitle[pathname] || "SandBox"
  
@@ -14,7 +14,7 @@ function Header({isOpen}) {
     <header className={`${isOpen ? "w-[calc(100%-12rem)]" : "w-[calc(100%-4rem)]"} h-12 fixed top-0 z-30 flex-2 border-b-* border-b border-b-[#0F172A] bg-[#F3F4F6]`}>
       <div className="">
      <div className='flex justify-between p-2'>
-      <h1 className='font-bold md:text-2xl text-[#0F172A]'>{title}</h1>
+      <h1 className='font-bold md:text-2xl text-[#0F172A] px-2'>{title}</h1>
        <div className='flex gap-2'>
         <div className='text-[#0F172A] pt-2'>
           <Bell />
