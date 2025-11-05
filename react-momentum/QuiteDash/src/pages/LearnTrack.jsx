f12ebe28d1e6448375b03ff88b120852645e2d2b
USER <aishamuarin@gmail.com>
2025-11-05 20:24:43 +0100

    style(learn): update styling on table

--- react-momentum/QuiteDash/src/pages/LearnTrack.jsx
+++ react-momentum/QuiteDash/src/pages/LearnTrack.jsx
@@ -14,9 +14,15 @@ function LearnTrack() {
   const handleNewRecords = (newRecord) => {
     
     const updatedRecords = [...records, {...newRecord, id:Date.now()}];
-    
     setRecords(updatedRecords);
-    window.dispatchEvent(new Event("statsUpdated"));
+    localStorage.setItem("learnRecords", JSON.stringify(updatedRecords));
+    
+    setTimeout(() => {
+      console.log("StatsUpdated event");
+      window.dispatchEvent(new Event("statsUpdated"));
+    }, 200);
+
+    
     
   }
   
@@ -27,18 +33,18 @@ function LearnTrack() {
 
 
   return (
-    <div className="h-screen bg-[#F3F4F6] p-4">
+    <div className="h-full bg-[#F3F4F6] p-4">
       <div>
         <div className="bg-[#0f1a32] rounded-lg text-white p-1.5 m-2">
           <h3 className="font-semibold text-xl px-2 py-1">Your Learning Footprints</h3>
-          <p className="text-sm my-1 px-2">Log learnning progress, track hours spent, and see your growth over time, with ideas that shape your journey. <br />
+          <p className="text-sm my-1 px-2">Log learning progress, track hours spent, and see your growth over time, with ideas that shape your journey. <br />
             Every new entry builds a record of your learning story.</p>
           <div className="flex justify-end px-4 items-center -my-2 mb-1">
              <button onClick={openForm}
              className="bg-white text-[#0F172A] px-1.5 py-1 rounded font-semibold hover:cursor-pointer">Note It</button>
              {openModal && (
               <Modal onClose={closeForm}>
-                <MyForm onAddRecord={handleNewRecords}/> 
+                <MyForm onAddRecord={handleNewRecords} closeForm={closeForm}/> 
               </Modal>
              )}
           </div>
@@ -46,17 +52,17 @@ function LearnTrack() {
         </div>
         
         
-        <div className="overflow-x-auto mt-6 p-2">
+        <div className="overflow-x-auto mt-6 px-2 py-2 rounded-2xl">
          {records.length > 0 ? (
-          <table className="min-w-full border border-[#0F172A] border-collapse text-[#0F172A] rounded-lg">
-            <thead className="bg-[#E2E8F0]">
+          <table className="min-w-full border border-[#0F172A] border-collapse text-[#0F172A]">
+            <thead className="bg-[#E2E8F0]  rounded-2xl">
               <tr className="border-b border-[#0F172A]">
-                <th className="border border-[#0F172A] p-1">Date</th>
-                <th className="border border-[#0F172A] p-1">Hours</th>
-                <th className="border border-[#0F172A] p-1">What I Learned</th>
-                <th className="border border-[#0F172A] p-1">Category</th>
-                <th className="border border-[#0F172A] p-1">Description</th>
-                <th className="border border-[#0F172A] p-1">Feature Built</th>
+                <th className="border border-[#0F172A] p-2">Date</th>
+                <th className="border border-[#0F172A] p-2">Hours</th>
+                <th className="border border-[#0F172A] p-2">What I Learned</th>
+                <th className="border border-[#0F172A] p-2">Category</th>
+                <th className="border border-[#0F172A] p-2">Description</th>
+                <th className="border border-[#0F172A] p-2">Feature Built</th>
                 
               </tr>
                  </thead>
