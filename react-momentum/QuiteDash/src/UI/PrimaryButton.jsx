2af8c4013d82c396986b978469f4746f4cfbc56a
USER <aishamuarin@gmail.com>
2025-11-13 14:36:17 +0100

    feat: create a reusable button to use across pages

---
+++ react-momentum/QuiteDash/src/UI/PrimaryButton.jsx
@@ -0,0 +1,12 @@
+
+function PrimaryButton({onClick}) {
+  return (
+      <div className="flex justify-end px-4 items-center -my-2 mb-1">
+      <button onClick={onClick} className="bg-white text-primary px-1.5 py-1 rounded font-semibold hover:cursor-pointer">
+      Note It
+      </button>
+    </div>
+  )
+}
+
+export default PrimaryButton
