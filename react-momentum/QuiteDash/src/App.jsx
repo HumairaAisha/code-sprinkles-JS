417b4923358c21949044b892307615f6a259c183
USER <aishamuarin@gmail.com>
2025-11-16 17:24:04 +0100

    import toaster to project

--- react-momentum/QuiteDash/src/App.jsx
+++ react-momentum/QuiteDash/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import Layout from './components/layout/Layout'
 import Dashboard from './pages/Dashboard'
 import LearnTrack from "./pages/LearnTrack";
@@ -7,6 +9,7 @@ import Resources from "./pages/Resources";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
 import Challenges from "./pages/Challenges";
+import ProjectHub from "./pages/ProjectHub";
 
 function App() {
 
@@ -21,12 +24,36 @@ function App() {
       <Route path="/milestone"  element={<Milestone/>}/>
       <Route path="/challenge" element={<Challenges/>}/>
       <Route path="/resources"  element={<Resources/>}/>
+      <Route path="/projectHub" element={<ProjectHub/>}/>
       <Route path="/profile"  element={<Profile/>}/>
       <Route path="/settings"  element={<Settings/>}/>
       
       </Route>
     </Routes>
-    
+    <Toaster position="top-center" reverseOrder={false}
+    toastOptions={{
+      success: {
+        duration:1500,
+        style: {
+          width: "320px",
+          background:"#0F172A",
+          color:"#fff",
+          padding: "12px 16px"
+
+        }
+      },
+      error: {
+        duration: 2500,
+        style: {
+          background:"#dc2626",
+          color: '#fff',
+          width: "300px",
+          padding: "12px 16px",
+          borderRadius: "10px"
+        }
+      }
+    }}
+    />
     </>
   )
 }
