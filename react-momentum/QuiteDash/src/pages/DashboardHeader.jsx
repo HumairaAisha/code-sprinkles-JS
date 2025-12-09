15963fc8e287ce88aabd0a75a62b8d7d3cfdc95b
USER <aishamuarin@gmail.com>
2025-12-09 20:49:34 +0100

    style(Dashboard-Intro): test style on dashboard intro component

--- react-momentum/QuiteDash/src/pages/DashboardHeader.jsx
+++ react-momentum/QuiteDash/src/pages/DashboardHeader.jsx
@@ -2,10 +2,10 @@
 function DashboardHeader() {
   return (
     <div className="pb-8">
-      <div className='bg-[#0f1a32] rounded-lg text-white'>
+      <div className='bg-[#0A1A29] rounded-lg text-white p-1'>
          {/* <h3 className='font-semibold text-lg py-1.5 px-4'>Stats That Tell a Story</h3> */}
          <h3 className="font-semibold text-lg px-4 py-2">Welcome Back, Aisha</h3>
-         <p className='pb-2 px-4'>See how your journey unfolds, each number is a reflection of your learning rhythm, progress, and milestones. <br />
+         <p className='text-sm pb-2 px-4'>See how your journey unfolds, each number is a reflection of your learning rhythm, progress, and milestones. <br />
             Behind every stat is the quiet effort that shapes your growth.</p>
       </div>
     </div>
