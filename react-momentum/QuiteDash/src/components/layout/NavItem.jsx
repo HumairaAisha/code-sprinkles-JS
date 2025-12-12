1d0111f2e4b1e14a471203a95a10f584e9273344
USER <aishamuarin@gmail.com>
2025-12-12 12:14:25 +0100

    style: modified text size on sidebar

--- react-momentum/QuiteDash/src/components/layout/NavItem.jsx
+++ react-momentum/QuiteDash/src/components/layout/NavItem.jsx
@@ -7,12 +7,12 @@ function NavItem({icon, title, path, showTitle}) {
    <>
    <NavLink to={path} 
    className={({ isActive }) =>
-    `flex items-center gap-2 px-3 py-1.5 rounded-md my-2
+    `flex items-center px-2 py-1.5 rounded-md my-2
      ${isActive ? "bg-gray-700 text-white" : "text-gray-300 hover:bg-gray-600"}` 
   }
   > {showTitle ? <div className="flex gap-2">
        <Icon  className="w-5 h-5" />
-      <span>{title}</span>
+      <span className="text-sm">{title}</span>
     </div> : <Icon className="w-5 h-5"/>}
     
   
