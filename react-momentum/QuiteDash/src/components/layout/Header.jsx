bfc1f04ccc0d74f39f2b3b35da3a3d608bbf1ae6
USER <aishamuarin@gmail.com>
2025-12-12 12:11:38 +0100

    style: test styling on header

--- react-momentum/QuiteDash/src/components/layout/Header.jsx
+++ react-momentum/QuiteDash/src/components/layout/Header.jsx
@@ -14,15 +14,15 @@ function Header({isOpen}) {
     <header className={`${isOpen ? "w-[calc(100%-13rem)]" : "w-[calc(100%-4rem)]"} h-12 fixed top-0 z-30 flex-2 border-b-* border-b border-b-[#0F172A] bg-[#F3F4F6]`}>
       <div className="">
      <div className='flex justify-between p-2'>
-      <h1 className='font-bold md:text-2xl text-[#0F172A] px-2'>{title}</h1>
+      <h1 className='font-bold md:text-2xl text-[#0A1A29] px-2'>{title}</h1>
        <div className='flex gap-2'>
-        <div className='text-[#0F172A] pt-2'>
+        <div className='text-[#0A1A29] pt-2'>
           <Bell />
         </div>
         <div className='text-[#0F172A] pt-2'>
           <Settings />
         </div>
-        <div className='text-white bg-[#0F172A] rounded-full p-2 text-lg'>
+        <div className='text-white bg-[#0A1A29] rounded-full p-2 text-lg'>
           <FaUser />
         </div>
       </div>
