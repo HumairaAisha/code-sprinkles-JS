37a886295ead542abff00c5137fdf9b1957c982a
USER <aishamuarin@gmail.com>
2025-12-16 12:59:17 +0100

    refactor(dashboard): useContext and import dashboardStatsContext to sync dashboard stats

--- react-momentum/QuiteDash/src/pages/Dashboard.jsx
+++ react-momentum/QuiteDash/src/pages/Dashboard.jsx
@@ -1,16 +1,23 @@
 
-import { useEffect, useState } from "react"
+//import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import StatsCard from "../components/StatsCard"
-import StatsData from "../components/data/stats"
 import DashboardHeader from "./DashboardHeader"
-import statData from "../components/data/stats"
+//import statData from "../components/data/stats"
+import Chart from "../components/data/Chart"
+import { useContext } from "react"
+import { DashboardStatsContext } from "../CustomHook/DashboardStatsContext"
+
 
   function Dashboard() {
-  const [stats, setStats] = useState(statData)
+
+     const {stats} = useContext(DashboardStatsContext)
+ /*  const [stats, setStats] = useState(statData)
   
     useEffect(() => {
       const updatesStatsFromStorage = () => {
+        console.log("stats updated event caught");
+        
         const learningTrack = JSON.parse(localStorage.getItem("learnRecords"))
         const challenges = JSON.parse(localStorage.getItem("challengeRecords")) || [];
         
@@ -18,35 +25,63 @@ import statData from "../components/data/stats"
         const hoursSpent = learningTrack.reduce((sum, item) => sum + (Number(item.hours) || 0), 0)
         const newConcept = learningTrack.length
         const challengesSolved = challenges.length;
+ */
+    /*      console.log("Updated values from localStorage:", {
+    hoursSpent,
+    newConcept,
+    challengesSolved,
+  });
+ */
 
-        setStats((prev) => prev.map((stat) => {
-          if (stat.title === "Hours Spent Coding") return {...stat, value:hoursSpent}
+  /* const storedStats = JSON.parse(localStorage.getItem("dashboardStats")) || statData
+  const titlesMatch = storedStats.every((item, index) => item.title === statData[index].title);
+
+if (!storedStats || storedStats.length !== statData.length || !titlesMatch) {
+  localStorage.setItem("dashboardStats", JSON.stringify(statData));
+  setStats(statData);
+} else {
+  setStats(storedStats);
+}
+
+  const updatedStats = storedStats.map((stat) => {
+    if (stat.title === "Hours Spent Coding") return {...stat, value:hoursSpent}
           if (stat.title === "New Concept Learned") return {...stat, value:newConcept}
           if (stat.title === "Challenges Solved") return {...stat, value:challengesSolved}
           return stat
-        })
-      )
+  })
+        
+          
+        localStorage.setItem("dashboardStats", JSON.stringify(updatedStats))
+        setStats(updatedStats)
+      
       }
-      updatesStatsFromStorage()
+      updatesStatsFromStorage();
+      
       window.addEventListener("statsUpdated", updatesStatsFromStorage);
-      return () => window.removeEventListener("statsUpdated", updatesStatsFromStorage)
-    },[])
+      window.addEventListener("storage", updatesStatsFromStorage)
+      return () => {
+        window.removeEventListener("statsUpdated", updatesStatsFromStorage)
+        window.removeEventListener("storage", updatesStatsFromStorage)
+      }
+    },[]) */
 
    
   return (
-    <div className="px-8 py-6 w-full h-screen bg-[#F3F4F6] ">
+    <div className="px-8 py-6 w-full h-full bg-[#F3F4F6] ">
      
       {/*  <Card /> */}
         <DashboardHeader />
  
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3  gap-y-6 gap-x-4">
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-y-6 gap-x-4 py-4">
          {stats.map((stat) => (
-            <div key={stat.id} className=" rounded-lg bg-gray-50 shadow hover:shadow-[#0F172A] hover:cursor-pointer">
+            <div key={stat.id} className="rounded-lg bg-gray-50 shadow hover:shadow-[#0F172A] hover:cursor-pointer">
                <StatsCard id={stat.id} title={stat.title} value={stat.value} notes={stat.notes} auto={stat.auto}/>
             </div>
          ))}
       </div>
-    
+    <div className="py-6">
+      <Chart/>
+    </div>
     </div>
   )
 }
