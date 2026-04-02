730ff693af967135fa4dcbf96f854755a030d025
USER <aishamuarin@gmail.com>
2026-04-02 21:33:16 +0100

    feat: create landing page component to render all landing page sections components

---
+++ react-momentum/QuiteDash/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,22 @@
+import HeroSection from "./Components/HeroSection"
+import NavBar from "./Components/NavBar"
+import Features from "./Components/Features"
+import Works from "./Components/Works"
+import About from "./Components/About"
+
+
+function LandingPage() {
+  return (
+   
+      <>
+      <NavBar/>
+     <HeroSection/>
+     <Features/>
+    <Works/>
+    <About />
+      </>
+    
+  )
+}
+
+export default LandingPage
