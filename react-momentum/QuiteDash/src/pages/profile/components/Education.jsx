af574c4db07d94c2e59e93cabf6b6aa68cffaf02
USER <aishamuarin@gmail.com>
2026-04-29 22:07:13 +0100

    style: add overflow to allow scroll to avoid height stretch on card

--- react-momentum/QuiteDash/src/pages/profile/components/Education.jsx
+++ react-momentum/QuiteDash/src/pages/profile/components/Education.jsx
@@ -3,14 +3,16 @@ import PrimaryButton from "../../../components/UI/PrimaryButton"
 
 function Education() {
   return (
-    <div>
-      <ReusableCard>
-   <div className="flex justify-between">
-      <h3 className="">Education/Experience</h3>
-      <PrimaryButton label={"Add"}/>
-   </div>
+      <ReusableCard className="h-full">
+   <div className="flex flex-col h-full">
+       <div className="flex justify-between items-center">
+          <h3>Education/Experience</h3>
+          <PrimaryButton label="Add" />
+        </div>
+   
    <div className="bg-[#3c4a69] w-full h-0.5 my-1.5"></div>
-   <div className="flex gap-1 py-1.5">
+  <div className="flex flex-col gap-1 overflow-y-auto max-h-48 pr-1">
+     <div className="flex gap-1 py-1.5">
      <div className="bg-[#3c4a69] w-0.5 h-8 my-1.5"></div>
      <div>
       <p className="font-semibold text-sm">Frontend Engineer</p>
@@ -25,8 +27,10 @@ function Education() {
      <p className="text-xs my-0.5">2018 - 2024</p>
      </div>
    </div>
+  </div>
+   </div>
       </ReusableCard>
-    </div>
+  
   )
 }
 
