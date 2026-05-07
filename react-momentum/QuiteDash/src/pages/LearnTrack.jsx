8bee170134e66fe8a613048ca1a89259a0aa8a10
USER <aishamuarin@gmail.com>
2026-05-07 23:21:29 +0100

    refactor(learn): fix dashboard stats to automatically updates from progress page without reloading dashboard page

--- react-momentum/QuiteDash/src/pages/LearnTrack.jsx
+++ react-momentum/QuiteDash/src/pages/LearnTrack.jsx
@@ -1,22 +1,17 @@
-
-import useLocalStorage from "../components/data/useLocalStorage"
 import { useContext, useState } from "react"
 import { DashboardStatsContext } from "../CustomHook/DashboardStatsContext"
 
 import Modal from "../components/Form/Modal"
-
 import PrimaryButton from "../components/UI/PrimaryButton"
 import Heading from "../components/UI/Heading"
 import ReusableCard from "../components/UI/ReusableCard"
 import DetailModal from "../components/UI/DetailModal"
 import ViewMoreButton from "../components/UI/ViewMoreButton"
-
 import LearnForm from "../components/Form/LearnForm"
 
 function LearnTrack() {
   
   const [openModal, setOpenModal] = useState()
-  
 
   const [selectedRecord, setSelectedRecord] = useState(null)
   const [openDetailModal, setOpenDetailModal] = useState(false)
@@ -37,22 +32,15 @@ const closeDetailModal = () => {
   const openLearningTrackerForm = () => setOpenModal(true)
   const closeLearningTrackerForm = () => setOpenModal(false)
 
-  const {recalculateStats} = useContext(DashboardStatsContext)
-
-   const [records, setRecords] = useLocalStorage("learnRecords",[])
+   const {learnRecords: records, setLearnRecords: setRecords} = useContext(DashboardStatsContext)
    
    const handleNewRecords = (newRecord) => {
     
-    const updatedRecords = [...records, {...newRecord, id:Date.now()}];
+   const updatedRecords = [...records, {...newRecord, id:Date.now()}];
     setRecords(updatedRecords);
-    
-
-    localStorage.setItem("learnRecords", JSON.stringify(updatedRecords));
-    
-    recalculateStats();
-    
   }
-  records.sort((a, b) => new Date(a.date) - new Date(b.date))
+  const sortedLearnTracker = [...records].sort((a, b) => new Date(a.date) - new Date(b.date))
+  
   
   /* const handleDelete = (id) => {
       removeRecord(id)
@@ -61,26 +49,27 @@ const closeDetailModal = () => {
 
 
   return (
-    <div className="h-srceen bg-[#F3F4F6] p-4">
+    <div className="h-screen bg-sandbox-ghost p-4">
       <div>
-        <div className="bg-[#0A1A29] rounded-lg text-white p-1.5 m-2">
+        <div className="bg-sandbox-navy rounded-lg text-sandbox-ghost p-2 m-2">
           <Heading
-        title={"Your Learning Footprints"}
-        text={"Log learning progress, track hours spent, and see your growth over time, with ideas that shape your journey."}
-        tagline={"Every new entry builds a record of your learning story."}
+        title={"Your Progress Footprints"}
+        text={"Log your activity, track hours spent, and see how your progress unfolds over time, with ideas that shape your journey."}
+        tagline={"Every new entry builds a record that tells a story."}
          />
-           <PrimaryButton onClick={openLearningTrackerForm}/>
+           <PrimaryButton label={"Note It"}
+           onClick={openLearningTrackerForm}/>
              {openModal && (
               <Modal onClose={closeLearningTrackerForm}>
-                <LearnForm key="challenge-form"
+                <LearnForm key="learning-form"
                 onAddRecord={handleNewRecords} closeForm={closeLearningTrackerForm}/> 
               </Modal>
              )}
         </div>
         <div className="mt-6 px-2 py-2 rounded-2xl">
-         {records.length > 0 ? (
+         {sortedLearnTracker.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-y-6 gap-x-4 py-4 p-2">
-            {records.map((record) => (
+            {sortedLearnTracker.map((record) => (
 
               <ReusableCard key={record.id}>
                 <p className="font-semibold py-0.5">Date: <span className="font-normal">{record.date}</span></p>
@@ -91,7 +80,7 @@ const closeDetailModal = () => {
             ))}
           </div>
          ) : ( <p className="text-center text-gray-600 py-4 italic">
-          No learning records yet. Click “Note It” to add one.
+          No progress records yet. Click “Note It” to add one.
           </p>)}
          
         </div>
@@ -104,9 +93,9 @@ const closeDetailModal = () => {
           { key: "hours", label: "Hours Spent" },
           { key: "category", label: "Category" },
           { key: "technology", label: "Technology" },
-          { key: "concept", label: "Concept" },
-          { key: "topic", label: "Concept Mastered" },
-          { key: "description", label: "Concept Description" },
+          { key: "focus", label: "Focus Area" },
+          { key: "outcome", label: "key outcome" },
+          { key: "notes", label: "Notes / Description" },
     ]}
           />
         )}
