ce8c14f0379294e7fbdc3cfe1748a286103ea0f1
USER <aishamuarin@gmail.com>
2026-05-13 20:56:17 +0100

    fix and lift state to layout so that both sidebar and header layout on mobile responsive view

--- react-momentum/QuiteDash/src/components/layout/Layout.jsx
+++ react-momentum/QuiteDash/src/components/layout/Layout.jsx
@@ -7,13 +7,18 @@ import { DashboardStatsProvider} from "../../CustomHook/DashboardProviderContext
 import { useState } from "react"
 function Layout() {
   const [isOpen, setIsOpen] = useState(true)
+  const [mobileNavOpen, setMobileNavOpen] = useState(false)
   return (
-    <div className="flex bg-[#F3F4F6]">
-      <SideBar isOpen={isOpen} setIsOpen={setIsOpen}
+    <div className="flex bg-sandbox-ghost">
+      <SideBar isOpen={isOpen} 
+      setIsOpen={setIsOpen}
+      mobileNavOpen={mobileNavOpen}
+      setMobileNavOpen={setMobileNavOpen}
        className="w-52" />
 
-      <div className={`${isOpen ? 'ml-52' : 'ml-16'} flex-1`}>
-        <Header isOpen={isOpen}/>
+      <div className={`${isOpen ? 'md:ml-52' : 'md:ml-16'} flex-1`}>
+        <Header isOpen={isOpen}
+        onMenuClick = {() => setMobileNavOpen(!mobileNavOpen)}/>
       <div className="flex-1 mt-12 bg-[#F3F4F6]">
       <DashboardStatsProvider>
         <Outlet />
