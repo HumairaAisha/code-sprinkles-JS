d68a1960a45bf2a6f098d29618f4eb4241601568
USER <aishamuarin@gmail.com>
2026-05-13 21:01:47 +0100

    refcator use label as props instead of hardcording button name(record it)

--- react-momentum/QuiteDash/src/components/UI/SecondaryButton.jsx
+++ react-momentum/QuiteDash/src/components/UI/SecondaryButton.jsx
@@ -1,9 +1,9 @@
 
-function SecondaryButton({onClick}) {
+function SecondaryButton({onClick, lable}) {
   return (
     <div className='flex justify-end'>
-      <button type='submit' onClick={onClick} className='bg-[#0A1A29] text-white font-semibold px-2 py-1.5 my-2 rounded hover:cursor-pointer'>
-         Record It
+      <button type='submit' onClick={onClick} className='bg-sandbox-navy text-sandbox-ghost font-semibold px-2 py-1.5 my-2 rounded hover:cursor-pointer'>
+         {lable}
          </button>
     </div>
   )
