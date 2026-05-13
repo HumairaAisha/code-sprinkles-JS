eaab8a5ddd569a9e04a74bdf69055aa926821eaa
USER <aishamuarin@gmail.com>
2026-05-13 21:00:16 +0100

    fix and use label as props instead on hardcoding button name

--- react-momentum/QuiteDash/src/components/UI/PrimaryButton.jsx
+++ react-momentum/QuiteDash/src/components/UI/PrimaryButton.jsx
@@ -1,9 +1,9 @@
 
-function PrimaryButton({onClick}) {
+function PrimaryButton({onClick, label}) {
   return (
-     <div className="flex justify-end px-4 items-center -my-2 mb-1">
-       <button onClick={onClick} className="bg-white text-[#0F172A] px-1.5 py-1 rounded font-semibold hover:cursor-pointer">
-      Note It
+     <div className="flex justify-end px-4 items-center -my-2 mb-1 pt-4 md:pt-0">
+       <button onClick={onClick} className="bg-sandbox-ghost text-sandbox-navy px-1.5 py-1 rounded font-semibold hover:cursor-pointer">
+      {label}
       </button>
      </div>
      
