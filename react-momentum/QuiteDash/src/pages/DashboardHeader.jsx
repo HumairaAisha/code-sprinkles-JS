de604b30be34296c8cbbf809202ded7863868197
USER <aishamuarin@gmail.com>
2026-05-14 21:37:49 +0100

    update UI style on haeder

--- react-momentum/QuiteDash/src/pages/DashboardHeader.jsx
+++ react-momentum/QuiteDash/src/pages/DashboardHeader.jsx
@@ -1,11 +1,11 @@
 
 function DashboardHeader() {
   return (
-    <div className="pb-8">
-      <div className='bg-[#0A1A29] rounded-lg text-white p-1'>
+    <div className="pb-2">
+      <div className='p-1'>
          {/* <h3 className='font-semibold text-lg py-1.5 px-4'>Stats That Tell a Story</h3> */}
-         <h3 className="font-semibold text-lg px-4 py-2">Welcome Back, Aisha</h3>
-         <p className='text-sm pb-2 px-4'>See how your journey unfolds, each number is a reflection of your learning rhythm, progress, and milestones. <br />
+         <h3 className="font-semibold text-xl px-4 py-2 text-sandbox-navy">Welcome Back</h3>
+         <p className=' pb-2 px-4 text-sandbox-navy'>See how your journey unfolds, each number is a reflection of your progress rhythm, progress, and milestones. <br />
             Behind every stat is the quiet effort that shapes your growth.</p>
       </div>
     </div>
