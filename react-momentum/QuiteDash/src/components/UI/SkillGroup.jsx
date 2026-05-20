1f709e74fdda5eefff200486e4b4b8c7aec10550
USER <aishamuarin@gmail.com>
2026-05-20 23:41:37 +0100

    Add input limit, button on input to handleAdd and fix layout issue on skill card

--- react-momentum/QuiteDash/src/components/UI/SkillGroup.jsx
+++ react-momentum/QuiteDash/src/components/UI/SkillGroup.jsx
@@ -1,24 +1,28 @@
 import ActionsButton from "./ActionsButton"
-import { useState } from "react"
+import { useState } from "react";
+import { Check } from 'lucide-react';
 
 
 function SkillGroup({label, items, variant, onAddSkill}) {
    const [showInput, setShowInput] = useState(false)
-   const [newSkill, setNewSkill] = useState("") // to add new
+   const [newSkill, setNewSkill] = useState("")// to add new
+
+   
 
    const showInlineInput = () => setShowInput(true)
 
    const handleAdd = () => {
+      
       const trimmed = newSkill.trim()
       if (trimmed) {
          onAddSkill(trimmed)
       }
       setNewSkill("")
-         setShowInput(false)
-      
+      setShowInput(false)
+     
    }
 
-   const handlekeyDown = (event) => {
+   const handleKeyDown = (event) => {
       if (event.key === "Enter") {
          handleAdd()
       }else if (event.key === "Escape") {
@@ -26,34 +30,50 @@ function SkillGroup({label, items, variant, onAddSkill}) {
          setShowInput(false)
       }
    }
+   const Max_Skill = 8;
+   const isLimit = (items ?? []).length >= Max_Skill;
+
   return (
     <div className="flex flex-col"> 
-      <div className="flex py-0.5 justify-between">
+     <div className="flex items-center justify-between">
+       <div className="flex items-center gap-3 flex-wrap">
       <p className="text-sm text-gray-800 font-semibold">{label}</p>
-     <ActionsButton actions={[
-      {label: "+", type:"ghost", onClick: showInlineInput}
-      ]}/>
-      </div>
-      <div className="flex flex-wrap gap-2">
-         {items.map((item) => (
+         <div className="flex flex-wrap gap-2">
+         {(items ?? []).map((item) => (
             <span key={item} className={`text-xs px-2 py-0.5 rounded-full ${variant === "primary" ? "bg-sandbox-navy/10 text-sandbox-navy" : "bg-gray-200 text-gray-500"}`}>
                {item}
             </span>
          ))}
+      </div>
+    
+     
+      </div>
       
-      {showInput && (
-         <input
+      <ActionsButton actions={[
+      {label: "+", type:"ghost", onClick: showInlineInput, disabled:isLimit}
+      ]}/>
+     </div>
+     {isLimit && (<p>Maximum of {Max_Skill} skills reached</p>)}
+
+      {/* inline input field */}
+      {showInput && !isLimit && (
+        <div className="relative flex items-center w-fit">
+          <input
          type="text"
          onChange={(event) => setNewSkill(event.target.value)}
          placeholder="Add new skill"
          value={newSkill}
-         className="text-xs px-3 py-1 border border-gray-300 rounded-full focus:outline-none focus:border-sandbox-navy min-w-[160px]"
+         className="text-xs px-3 py-1 border border-gray-300 rounded-full focus:outline-none focus:border-sandbox-navy min-w-[160px] pr-11"
          onBlur={handleAdd}
-         onKeyDown={handlekeyDown}
+         onKeyDown={handleKeyDown}
          autoFocus
          />
+         <button onMouseDown={handleAdd} className="absolute right-0 top-1/2 -translate-y-1/2 rounded-full bg-sandbox-navy text-sandbox-ghost hover:bg-sandbox-navy/80 hover:cursor-pointer">
+           <Check />
+         </button>
+        </div>
       )}
-      </div>
+      
     </div>
   )
 }
