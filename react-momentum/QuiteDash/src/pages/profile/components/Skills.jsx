acb2a4710a4d11499b5447ee3cf9d5a8f2f8d836
USER <aishamuarin@gmail.com>
2026-05-20 23:44:32 +0100

    refactor and remove hardcoded inputs to allow skill to be and implement data persistent to save data to localStorage

--- react-momentum/QuiteDash/src/pages/profile/components/Skills.jsx
+++ react-momentum/QuiteDash/src/pages/profile/components/Skills.jsx
@@ -1,17 +1,42 @@
 import PrimaryButton from "../../../components/UI/PrimaryButton";
 import ReusableCard from "../../../components/UI/ReusableCard";
-
-
+import SkillGroup from "../../../components/UI/SkillGroup";
+import useLocalStorage from "../../../components/data/useLocalStorage";
 
 function Skills() {
+  const [skillGroups, setSkillGroups] = useLocalStorage ("skillGroup", [
+  
+    {id: "primary-stack", label: "Primary Stack", items: [], variant: "primary"},
+    {id: "tools", label: "Tools", items: [], variant: "secondary"},
+    {id: "familiar-with", label: "Familiar With", items: [], variant: "secondary"},
+    {id: "exploring", label: "Exploring", items: [], variant: "secondary"}
+ ])
+
+  const handleAddSkill = (skillId, newSkill) => {
+    setSkillGroups((prev) => prev.map((skill) => (
+      skill.id === skillId ? {...skill, items: [...skill.items, newSkill]} : skill
+    )))
+  }
   return (
        <ReusableCard className="h-full">
    <div className="flex justify-between">
       <h3 className="text-sandbox-navy">Skills</h3>
-      <PrimaryButton label={"Add"}/>
    </div>
    <div className="bg-[#3c4a69] w-full h-0.5 my-1.5"></div>
-   <div className="flex flex-wrap items-center gap-4 my-2">
+    {skillGroups.map((skill) => (
+        <SkillGroup
+        key={skill.id}
+      label={skill.label}
+      items={skill.items}
+      variant={skill.variant}
+      onAddSkill={(newSkill) => handleAddSkill(skill.id, newSkill)}
+      />
+   
+
+    ))}
+
+
+  {/*  <div className="flex flex-wrap items-center gap-4 my-2">
       <span className="text-sm text-gray-800 font-semibold">Primary Stack</span>
   {["React", "TypeScript", "Tailwind CSS"].map((skill) => (
     <span key={skill} className="text-xs bg-sandbox-navy/10 text-sandbox-navy px-2 py-0.5 rounded-full">
@@ -43,7 +68,7 @@ function Skills() {
       {tool}
     </span>
   ))}
-</div>
+</div> */}
 
       </ReusableCard>
     
