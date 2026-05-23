87ebc02afebadc12ca6df58560414761ee51b0d9
USER <aishamuarin@gmail.com>
2026-05-23 09:48:01 +0100

    Refactor to show only filled fields to avoid empty input on cards

--- react-momentum/QuiteDash/src/components/UI/DetailModal.jsx
+++ react-momentum/QuiteDash/src/components/UI/DetailModal.jsx
@@ -3,23 +3,23 @@ function DetailModal({data, fields, onClose}) {
   return (
 
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-    <div className="bg-white rounded-lg max-w-lg shadow-lg overflow-y-auto">
+    <div className="bg-white rounded-lg max-w-xl shadow-lg overflow-y-auto">
       <div className='flex justify-end p-2'>
    <button onClick={onClose} className='hover:cursor-pointer '>
      <X/>
    </button>
       </div>
       <h2 className='text-center font-semibold text-lg'>Details</h2>
-      
-
     
        <div className='p-2'>
-         {fields.map((field) => (
-      <div key={field.key}>
-         <p className="font-semibold py-0.5 text-[#0A1A29]">{field.label}: <span className="font-normal">{data[field.key]}</span></p>
-         
-      </div>
-   ))}
+    {fields.map((field) => (
+      data[field.key] && (
+        <div key={field.key}>
+      <p className='font-semibold py-0.5 text-[#0A1A29]'>{field.label}: <span className='font-normal'>{data[field.key]}</span> </p>
+    </div>
+      )
+      
+    ) )}
        </div>
   
     </div>
