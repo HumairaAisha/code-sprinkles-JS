f36b3e1eea852794dfd9913faec457720c83c9a2
USER <aishamuarin@gmail.com>
2026-05-23 09:20:15 +0100

    Add pagination to page and functions to handle edit and delete actions on cards

--- react-momentum/QuiteDash/src/pages/LearnTrack.jsx
+++ react-momentum/QuiteDash/src/pages/LearnTrack.jsx
@@ -8,6 +8,8 @@ import ReusableCard from "../components/UI/ReusableCard"
 import DetailModal from "../components/UI/DetailModal"
 import ViewMoreButton from "../components/UI/ViewMoreButton"
 import LearnForm from "../components/Form/LearnForm"
+import Pagination from "../components/UI/Pagination"
+import toast from "react-hot-toast"
 
 function LearnTrack() {
   
@@ -15,6 +17,10 @@ function LearnTrack() {
 
   const [selectedRecord, setSelectedRecord] = useState(null)
   const [openDetailModal, setOpenDetailModal] = useState(false)
+  const [isEditing, setIsEditing] = useState(null)
+  const [itemToDelete, setItemToDelete] = useState(null)
+  const [confirmOpen, setconfirmOpen] = useState(false)
+
 
   const handleViewMore = (record) => {
   setSelectedRecord(record)
@@ -26,11 +32,19 @@ const closeDetailModal = () => {
   setOpenDetailModal(false)
 }
 
-  //const openLearningDetailsInfo = () => setOpenCard(true)
-  //const closeLearningDetailsInfo = () => setOpenCard(false)
 
-  const openLearningTrackerForm = () => setOpenModal(true)
-  const closeLearningTrackerForm = () => setOpenModal(false)
+  const openLearningTrackerForm = () => { 
+    setOpenModal(true)
+    setIsEditing(null)
+  }
+  const closeLearningTrackerForm = () => {
+    setOpenModal(false)
+    setIsEditing(null)
+  }
+  const openEditLearningForm = (item) => {
+    setIsEditing(item)
+    setOpenModal(true)
+  }
 
    const {learnRecords: records, setLearnRecords: setRecords} = useContext(DashboardStatsContext)
    
@@ -39,17 +53,30 @@ const closeDetailModal = () => {
    const updatedRecords = [...records, {...newRecord, id:Date.now()}];
     setRecords(updatedRecords);
   }
+
+  const handleEdit = (updatedRecord) => {
+    setRecords((learn) => [...learn, {...updatedRecord, id: Date.now()}])
+    toast.success("Update Successful")
+    closeLearningTrackerForm()
+  }
+
+  const handleConfrimDelete = () => {
+    setRecords((learn) => learn.filter((item) => item.id !== ))
+  }
   const sortedLearnTracker = [...records].sort((a, b) => new Date(a.date) - new Date(b.date))
   
-  
-  /* const handleDelete = (id) => {
-      removeRecord(id)
-  } */
+  const recordsPerPage = 12 
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const lastIndex = currentPage * recordsPerPage
+  const firstIndex = lastIndex - recordsPerPage
+  const paginatedRecords = sortedLearnTracker.slice(firstIndex, lastIndex)
+  const totalPages = Math.ceil(sortedLearnTracker.length / recordsPerPage)
 
 
 
   return (
-    <div className="h-screen bg-sandbox-ghost p-4">
+    <div className="min-h-screen bg-sandbox-ghost p-4">
       <div>
         <div className="bg-sandbox-navy rounded-lg text-sandbox-ghost p-2 m-2">
           <Heading
@@ -67,9 +94,9 @@ const closeDetailModal = () => {
              )}
         </div>
         <div className="mt-6 px-2 py-2 rounded-2xl">
-         {sortedLearnTracker.length > 0 ? (
+         {paginatedRecords.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-y-6 gap-x-4 py-4 p-2">
-            {sortedLearnTracker.map((record) => (
+            {paginatedRecords.map((record) => (
 
               <ReusableCard key={record.id}>
                 <p className="font-semibold py-0.5">Date: <span className="font-normal">{record.date}</span></p>
@@ -82,7 +109,11 @@ const closeDetailModal = () => {
          ) : ( <p className="text-center text-gray-600 py-4 italic">
           No progress records yet. Click “Note It” to add one.
           </p>)}
-         
+         <Pagination
+         currentPage={currentPage}
+         totalPages={totalPages}
+         onPageChange={setCurrentPage}
+         />
         </div>
         {openDetailModal && selectedRecord && (
           <DetailModal
