3ebf67252477dc915261cb5ff0182b48f5545689
USER <aishamuarin@gmail.com>
2026-06-13 22:47:29 +0100

    Add navigation to dashboard when user exits

--- react-momentum/QuiteDash/src/pages/LandingPage/LandingPage.jsx
+++ react-momentum/QuiteDash/src/pages/LandingPage/LandingPage.jsx
@@ -4,9 +4,16 @@ import Features from "./Components/Features"
 import Works from "./Components/Works"
 import About from "./Components/About"
 import Footer from "./Components/Footer"
-
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 function LandingPage() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (localStorage.getItem('user') !== null) {
+      navigate('/dashboard')
+    }
+  }, [])
   return (
    
       <>
