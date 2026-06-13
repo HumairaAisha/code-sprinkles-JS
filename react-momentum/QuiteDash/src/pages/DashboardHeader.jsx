4419e6e63f66f1198a99577a6cd399f57dd111f6
USER <aishamuarin@gmail.com>
2026-06-13 22:54:14 +0100

    read user and display first name from localstorage

--- react-momentum/QuiteDash/src/pages/DashboardHeader.jsx
+++ react-momentum/QuiteDash/src/pages/DashboardHeader.jsx
@@ -1,11 +1,13 @@
 
 function DashboardHeader() {
+  const user = JSON.parse(localStorage.getItem('user'))
+  const firstName = user?.name ? user.name.split(' ')[0] : null
   return (
     <div className="pb-2">
       <div className='p-1'>
-         {/* <h3 className='font-semibold text-lg py-1.5 px-4'>Stats That Tell a Story</h3> */}
-         <h3 className="font-semibold text-xl px-4 py-2 text-sandbox-navy">Welcome Back</h3>
-         <p className=' pb-2 px-4 text-sandbox-navy'>See how your journey unfolds, each number is a reflection of your progress rhythm, progress, and milestones. <br />
+       
+         <h3 className="font-semibold text-xl py-2 text-sandbox-navy"> {user ? `Welcome Back ${firstName}`  : "Welcome"}</h3>
+         <p className='pb-2  text-sandbox-navy text-sm md:text-lg'>See how your journey unfolds, each number is a reflection of your progress rhythm, and milestones. <br />
             Behind every stat is the quiet effort that shapes your growth.</p>
       </div>
     </div>
